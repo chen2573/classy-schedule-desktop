ac477db26df808fa56424b56d6a6a5e5dc6740f0
Anshul Bharath <anshul.bharath@gmail.com>
2022-03-05 13:44:04 -0600

    Getting Course hook to add data to App state.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Home from './home.js';
-import Course from './course.js';
+import Course from './components/Course.js';
 import Professor from './professor.js';
 import Room from './room.js';
 import Solution from './solution.js';
 
 
+
 function App() {
-  return (
+   const [courses, setCourse] = useState([])
+
+   const addCourse = (course) => {
+       const id = Math.floor(Math.random() * 10000) + 1
+
+        const newCourse = { id, ...course}
+        setCourse([...courses, newCourse])
+   }
+  
+    return (
     <Router>
       <div className="App">
         <Home/>
         <Routes>
-          <Route path='/course' element={<Course/>} />
+          <Route path='/course' element={<Course onAddCourse={addCourse}/>} />
           <Route path='/professor' element={<Professor/>} />
           <Route path='/room' element={<Room/>} />
           <Route path='/schedule' element={<Solution/>} />
