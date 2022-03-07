4b9c80cb23e1358023d6808c8b26789d18709125
Anshul Bharath <anshul.bharath@gmail.com>
2022-03-06 20:40:17 -0600

    Refectoring CourseAddPage and making small tweaks.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Home from './home.js';
-import CoursePage from './components/CoursePage.js';
+import CoursePage from './components/CourseAddPage.js';
 import Professor from './professor.js';
 import Room from './room.js';
 import Solution from './solution.js';
@@ -10,43 +10,40 @@ import Solution from './solution.js';
 const DEVELOPMENT_MODE = false; // Change to true when you want to debug with dummy data.
 
 function App() {
-  const [courses, setCourse] = useState([])
+  const [courses, setCourses] = useState([])
+  
 
   const addCourse = (course) => {
     const id = Math.floor(Math.random() * 10000) + 1
 
     const newCourse = { id, ...course }
-    setCourse([...courses, newCourse])
+    setCourses([...courses, newCourse])
   }
 
-  const testDB = () => {
+  const testDBQuery = () => {
     if (window.DB === undefined || DEVELOPMENT_MODE) {
       console.log('Using dummy data')
     }
     else {
+      // Send a query to main
       window.DB.send("toMain", "some data");
 
-      window.DB.receive("fromMain", (data) => {
-        console.log(data);
-        console.log(typeof data)
-        console.log(data[0].ClassName)
+      // Recieve the results
+      window.DB.receive("fromMain", (dataRows) => {
+        console.log(dataRows);
 
-        for(var i=0; i<data.length; i++) {
-          var course = data[i];
+        dataRows.forEach(data => {
+          var id = data.ClassId;
+          var program = data.ClassName;
+          var courseCapacity = data.Capacity;
+          var number = data.CourseNumber;
 
-          var id = course.ClassId;
-          var program = course.ClassName;
-          var courseCapacity = course.Capacity;
-          var number = course.CourseNumber;
-
-          var newCourse = {program, number};
-          setCourse([...courses, newCourse])
-
-        }
+          var newCourse = {program, number}; //This needs to be the same as onAddCourse() in CourseAddPage.js
+          setCourses([...courses, newCourse])
+        })
       });
     }
   }
-
 
   return (
     <Router>
@@ -59,7 +56,7 @@ function App() {
           <Route path='/schedule' element={<Solution />} />
         </Routes>
 
-        <button onClick={testDB}>SEND</button>
+        <button onClick={testDBQuery}>SEND</button>
       </div>
     </Router>
   );
