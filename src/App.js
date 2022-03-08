c1844cb064a377ba450c2328f4da3e132c829188
Anshul Bharath <anshul.bharath@gmail.com>
2022-03-08 17:40:27 -0600

    Getting database query to auto-refresh once page is loaded.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const DEVELOPMENT_MODE = true; // Change to true when you want to debug with dum
 function App() {
   const [programs, setPrograms] = useState([]) //This has to be an Array for some reason.
   const [courses, setCourses] = useState([])
-
   const [professors, setProfessors] = useState([])
 
 
@@ -32,61 +31,84 @@ function App() {
     setCourses(courses.filter((course) => course.id !== id))
   }
   
-  /**
-   * Initial refresh of course data from both database and sample data.
-   */
-  useEffect( () => {
+ 
   const addProfessor = (professor) => {
     const id = Math.floor(Math.random() * 10000) + 1
 
     const newProfessor = { id, ...professor }
     setProfessors([...professors, newProfessor])
   }
+
   const deleteProfessor = (id) => {
     console.log('delete',id)
   }
-  const testDBQuery = () => {
-    if (window.DB === undefined || DEVELOPMENT_MODE) {
-      console.log('Using dummy data')
 
-      sampleCourses.forEach(data => {
-          var courseID = data.courseID;
-          var program = data.program;
-          var capacity = data.capacity;
-          var number = data.number;
-          var name = data.name;
-          const id = Math.floor(Math.random() * 10000) + 1
+    /**
+   * Initial refresh of course data from both database and sample data.
+   */
+  useEffect(() => {
+      // Clears up the currently stored data and gets new data in the following code.
+      // There was a bug where with every refresh, we would get duplicate state.
+      //setCourses('')
+      setPrograms('')
 
-          var newCourse = {id, program, number, name, courseID, capacity}; //This needs to be the same as onAddCourse() in CourseAddPage.js
-          setCourses([...courses, newCourse])
+      if (window.DB === undefined || DEVELOPMENT_MODE) {
+        console.log('Using dummy data')
 
+        console.log(typeof sampleCourses)
+        console.log(sampleCourses)
+
+        for (let key in sampleCourses){
+            let courseID = sampleCourses[key].courseID;
+            let program = sampleCourses[key].program;
+            let capacity = sampleCourses[key].capacity;
+            let number = sampleCourses[key].number;
+            let name = sampleCourses[key].name;
+            let id = Math.floor(Math.random() * 10000) + 1
+
+            var newCourse = {id, program, number, name, courseID, capacity};
+            setCourses([...courses, newCourse])
+        }
+        
+        // sampleCourses.forEach(data => {
+        //   var courseID = data["courseID"];
+        //   var program = data["program"];
+        //   var capacity = data["capacity"];
+        //   var number = data["number"];
+        //   var name = data["name"];
+        //   const id = Math.floor(Math.random() * 10000) + 1
+
+        //   const newCourse = {id, program, number, name, courseID, capacity}; //This needs to be the same as onAddCourse() in CourseAddPage.js
+        //   setCourses([...courses, newCourse])
+        // })
+        setPrograms(samplePrograms)
+      }
+      else {
+        // Send a query to main
+        window.DB.send("toMain", "some data");
+
+        // Recieve the results
+        window.DB.receive("fromMain", (dataRows) => {
+          console.log(dataRows);
+          console.log(typeof dataRows);
+
+          dataRows.forEach(data => {
+            var courseID = data.ClassID;
+            var program = data.department;
+            var department = data.department;
+            var capacity = data.Capacity;
+            var number = data.CourseNumber;
+            var name = data.ClassName;
+            const id = Math.floor(Math.random() * 10000) + 1
+
+            var newCourse = {program, number, name, courseID, capacity}; //This needs to be the same as onAddCourse() in CourseAddPage.js
+            var newProfessor = {department, name};
+            setCourses([...courses, newCourse])
+            setProfessors([...professors, newProfessor])
+          })
           setPrograms(samplePrograms)
-      })
-    }
-    else {
-      // Send a query to main
-      window.DB.send("toMain", "some data");
-
-      // Recieve the results
-      window.DB.receive("fromMain", (dataRows) => {
-        console.log(dataRows);
-
-        dataRows.forEach(data => {
-          var courseID = data.ClassID;
-          var program = data.department;
-          var department = data.department;
-          var capacity = data.Capacity;
-          var number = data.CourseNumber;
-          var name = data.ClassName;
-          const id = Math.floor(Math.random() * 10000) + 1
-
-          var newCourse = {program, number, name, courseID, capacity}; //This needs to be the same as onAddCourse() in CourseAddPage.js
-          var newProfessor = {department, name};
-          setCourses([...courses, newCourse])
-          setProfessors([...professors, newProfessor])
-        })
-      });
-    }
+        });
+      }
   }, [])
 
 
@@ -102,8 +124,6 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/course' element={<CoursePage onDelete={deleteCourse} onAddCourse={addCourse} courses={courses} programs={programs}/>} />
-          <Route path='/professor' element={<Professor />} />
-          <Route path='/course' element={<CoursePage onDelete={deleteCourse} onAddCourse={addCourse} courses={courses} />} />
           <Route path='/professor' element={<ProfessorPage onDelete={deleteProfessor} onAddProfessor={addProfessor} professors={professors} />} />
           <Route path='/room' element={<Room />} />
           <Route path='/schedule' element={<Solution />} />
