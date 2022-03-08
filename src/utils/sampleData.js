6717472b164a1b5841c85291e0d9c290db3ed309
Anshul Bharath <anshul.bharath@gmail.com>
2022-03-08 16:09:41 -0600

    Adding sample data to use for dev.

---
+++ src/utils/sampleData.js
@@ -0,0 +1,53 @@
+/**
+ * Sample Data for Courses. Feel free to add and adjust
+ */
+export const sampleCourses = [
+    {
+        "program": "CISC",
+        "number": 131,
+        "name": "Intro to Programming",
+        "courseID": 1234,
+        "capacity": 20
+    },
+    {
+        "program": "CISC",
+        "number": 230,
+        "name": "Object Oriented Programming",
+        "courseID": 1233,
+        "capacity": 20
+    },
+    {
+        "program": "STAT",
+        "number": 200,
+        "name": "Statistics",
+        "courseID": 1232,
+        "capacity": 20
+    },
+    {
+        "program": "STAT",
+        "number": 440,
+        "name": "Data Mining",
+        "courseID": 1234,
+        "capacity": 20
+    }
+]
+
+export const samplePrograms = ["STAT", "MATH", "GEO", "MUSC"]
+
+/**
+ * Sample data for professors.
+ */
+export const sampleProfessors = [
+    {
+        
+    }
+]
+
+/**
+ * Sample data for rooms.
+ */
+export const sampleRooms = [
+    {
+        
+    }
+]
