f07b21092c99245ab53b9266aea9d58f39685cbd
Anshul Bharath <anshul.bharath@gmail.com>
2022-03-11 20:46:08 -0600

    Fixing some formatting.

--- public/main.js
+++ public/main.js
@@ -4,10 +4,17 @@ const path = require('path');
 const shell = require('electron').shell;
 const mysql = require('mysql');
 const fs = require('fs');
-const { ipcRenderer } = require('electron');
+const {
+    ipcRenderer
+} = require('electron');
 
 // Objects coming from electron
-const {app, BrowserWindow, Menu, ipcMain } = electron;
+const {
+    app,
+    BrowserWindow,
+    Menu,
+    ipcMain
+} = electron;
 
 // !!! SET process environment. Comment this out if packaging for development!!!
 //process.env.NODE_ENV = 'production';
@@ -16,42 +23,39 @@ let mainWindow;
 let addWindow;
 
 // Listen for app to ready
-app.on('ready', function() 
-{
+app.on('ready', function () {
     createMainWindow();
 });
 
 function createMainWindow() {
     // Create new window
-    mainWindow = new BrowserWindow(
-        {
-            // This is to allow node code to run in html
-            webPreferences: 
-            {
-                nodeIntegration: false,
-                enableRemoteModule: true,
-                contextIsolation: true,
-                preload: path.join(__dirname, 'preload.js')
-            },
-            width: 1000,
-            height: 800
-        });
-    
-        // Load main.html into window
-        // This syntax is just //__dirname/mainWindow.html
-        // __dirname gets the relative path of THIS file (main.js)
-        mainWindow.loadURL('http://localhost:3000');
-    
-        // Quit entire application when main process is closed
-        mainWindow.on('closed', function(){
-            app.quit();
-        });
-    
-        // Build menu from template
-        const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    
-        // Insert menu
-        Menu.setApplicationMenu(mainMenu);
+    mainWindow = new BrowserWindow({
+        // This is to allow node code to run in html
+        webPreferences: {
+            nodeIntegration: false,
+            enableRemoteModule: true,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
+        },
+        width: 1000,
+        height: 800
+    });
+
+    // Load main.html into window
+    // This syntax is just //__dirname/mainWindow.html
+    // __dirname gets the relative path of THIS file (main.js)
+    mainWindow.loadURL('http://localhost:3000');
+
+    // Quit entire application when main process is closed
+    mainWindow.on('closed', function () {
+        app.quit();
+    });
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
 }
 
 // Handle create add window 
@@ -59,14 +63,13 @@ function createMainWindow() {
 function createAddWindow() {
     addWindow = new BrowserWindow({
         // This is to allow node code to run in html
-        webPreferences: 
-        {
+        webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
         },
         width: 300,
         height: 200,
-        title:'Add Shopping List Item',
+        title: 'Add Shopping List Item',
         parent: mainWindow,
         modal: true
     });
@@ -75,30 +78,28 @@ function createAddWindow() {
     // This syntax is just //__dirname/mainWindow.html
     addWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'addWindow.html'),
-        protocol:'file:',
+        protocol: 'file:',
         slashes: true
     }));
 
     // Garbage collection handle
-    addWindow.on('close', function(){
+    addWindow.on('close', function () {
         addWindow = null;
     });
 }
 
 // Create menu template
-const mainMenuTemplate = [
-    {
-        label:'File',
-        submenu: [
-            {
+const mainMenuTemplate = [{
+        label: 'File',
+        submenu: [{
                 label: 'Add Item',
-                click(){
+                click() {
                     createAddWindow();
                 }
             },
             {
                 label: 'Clear Items',
-                click(){
+                click() {
                     mainWindow.webContents.send('item:clear');
                 }
             },
@@ -106,43 +107,40 @@ const mainMenuTemplate = [
                 type: 'separator'
             },
             {
-                label:'Quit',
+                label: 'Quit',
                 accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q', // Adds hotkey of Q
-                click(){
+                click() {
                     app.quit();
                 }
             }
         ]
     },
     {
-        label:'Help',
-        submenu: [
-            {
-                label: 'About',
-                click() {
-                    shell.openExternal('https://github.com/chen2573/CapstoneDesktopDev01#readme');
-                }
-
+        label: 'Help',
+        submenu: [{
+            label: 'About',
+            click() {
+                shell.openExternal('https://github.com/chen2573/CapstoneDesktopDev01#readme');
             }
-        ]
+
+        }]
     }
-    
+
 ];
 
 // If mac, add empty object to menu template
-if(process.platform == 'dawrwin') {
+if (process.platform == 'dawrwin') {
     mainMenuTemplate.unshift({});
 }
 
 // Add developer tools item if not in production
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
     mainMenuTemplate.push({
         label: 'Developer Tools',
-        submenu:[
-            {
+        submenu: [{
                 label: 'Toggle Dev Tools',
                 accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I', // Adds hotkey of I
-                click(item, focusedWindow){
+                click(item, focusedWindow) {
                     focusedWindow.toggleDevTools();
                 }
             },
@@ -170,46 +168,42 @@ ipcMain.on("toMain:Course", (event, args) => {
 
 // ============ DataBase functions ======================== 
 const connectToSever = () => {
-    return new Promise((resolve, reject)=> {
-        var config =
-    {
-        host: 'capstonedb01.mysql.database.azure.com',
-        user: 'desktopteam',
-        password: 'desktoppass',
-        database: 'classyschedule',
-        port: 3306,
-        //ssl: {ca: fs.readFileSync(path.join(__dirname, 'DBCertificate', 'DigiCertGlobalRootCA.crt.pem'))}
-    };
+    return new Promise((resolve, reject) => {
+        var config = {
+            host: 'capstonedb01.mysql.database.azure.com',
+            user: 'desktopteam',
+            password: 'desktoppass',
+            database: 'classyschedule',
+            port: 3306,
+            //ssl: {ca: fs.readFileSync(path.join(__dirname, 'DBCertificate', 'DigiCertGlobalRootCA.crt.pem'))}
+        };
 
-    const conn = new mysql.createConnection(config);
+        const conn = new mysql.createConnection(config);
 
-    conn.connect(
-        function (err) { 
-        if (err) { 
-            console.log("!!! Cannot connect !!! Error:");
-            reject(err)
-        }
-        else
-        {
-            resolve(conn)
-        }
-    });
+        conn.connect(
+            function (err) {
+                if (err) {
+                    console.log("!!! Cannot connect !!! Error:");
+                    reject(err)
+                } else {
+                    resolve(conn)
+                }
+            });
     })
-    
+
 }
 
-function queryDatabase(query){
+function queryDatabase(query) {
     return new Promise((resolve, reject) => {
         connectToSever().then(conn => {
-            conn.query(query, function(err, results, fields) {
-                if(err) {
+            conn.query(query, function (err, results, fields) {
+                if (err) {
                     reject(err);
-                }
-                else {
+                } else {
                     resolve(results);
                 }
             })
-     
+
         })
     })
 }
--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import ProfessorPage from './components/ProfessorAddPage.js';
 import RoomPage from './components/RoomAddPage.js'
 import Solution from './components/Solution.js';
 import MenuBar from './components/Menubar.js';
-import {createTheme, ThemeProvider} from '@mui/material';
-import {sampleCourses, samplePrograms, sampleProfessors, sampleRooms} from './utils/sampleData';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { sampleCourses, samplePrograms, sampleProfessors, sampleRooms } from './utils/sampleData';
 import varValueConvert from 'cross-env/src/variable';
 
 /**
@@ -55,10 +55,10 @@ function App() {
   const [professors, setProfessors] = useState([]);
   const [rooms, setRooms] = useState([]);
 
-//#region Course crud operations
+  //#region Course crud operations
   const addCourse = (course) => {
     let programIdArray = programs.filter((program) => {
-      if(program.programName === course.program) {
+      if (program.programName === course.program) {
         return program.programId;
       }
 
@@ -74,7 +74,7 @@ function App() {
       window.DB.send(CHANNEL_COURSE_TO_MAIN, query);
     }
   };
-  
+
   const deleteCourse = (id) => {
     console.log(id);
     setCourses(courses.filter((course) => course.id !== id));
@@ -86,8 +86,8 @@ function App() {
     }
   };
 
-//#endregion  
- 
+  //#endregion  
+
   const addProfessor = (professor) => {
     const id = Math.floor(Math.random() * 10000) + 1;
 
@@ -96,7 +96,7 @@ function App() {
   };
 
   const deleteProfessor = (id) => {
-    console.log('delete',id);
+    console.log('delete', id);
     setProfessors(professors.filter((professor) => professor.id !== id));
   };
 
@@ -108,14 +108,14 @@ function App() {
   };
 
   const deleteRoom = (id) => {
-    console.log('delete',id);
+    console.log('delete', id);
     setRooms(rooms.filter((room) => room.id !== id));
   };
 
-    /**
-   * Gets the latest data for all entities when a new page is loaded.
-   */
-  useEffect(updateAllStates,[]);
+  /**
+ * Gets the latest data for all entities when a new page is loaded.
+ */
+  useEffect(updateAllStates, []);
 
   function updateAllStates() {
     getLatestPrograms();
@@ -127,7 +127,7 @@ function App() {
   /**
    * Gets the latest data for courses.
    */
-   function getLatestPrograms() {
+  function getLatestPrograms() {
     // Clears up the currently stored data and gets new data in the following code.
     // There was a bug where with every refresh, we would get duplicate state.
     //setCourses('')
@@ -138,12 +138,12 @@ function App() {
       console.log('Using sample data');
 
       samplePrograms.map((program) => {
-          let programId = program.id;
-          let programName = program.name;
-          const id = Math.floor(Math.random() * 10000) + 1
+        let programId = program.id;
+        let programName = program.name;
+        const id = Math.floor(Math.random() * 10000) + 1
 
-          let newProgram = {id, programId, programName};
-          statePrograms.push(newProgram);
+        let newProgram = { id, programId, programName };
+        statePrograms.push(newProgram);
       });
       setPrograms(statePrograms);
     }
@@ -161,73 +161,73 @@ function App() {
           let programName = program.dept_name;
           const id = Math.floor(Math.random() * 10000) + 1;
 
-          let newProgram = {id, programId, programName};
+          let newProgram = { id, programId, programName };
           statePrograms.push(newProgram);
         });
         setPrograms(statePrograms);
       });
     }
-}
+  }
 
   /**
    * Gets the latest data for courses.
    */
   function getLatestCourses() {
-      // Clears up the currently stored data and gets new data in the following code.
-      // There was a bug where with every refresh, we would get duplicate state.
-      //setCourses('')
-      let stateCourses = [];
+    // Clears up the currently stored data and gets new data in the following code.
+    // There was a bug where with every refresh, we would get duplicate state.
+    //setCourses('')
+    let stateCourses = [];
 
-      if (window.DB === undefined || DEVELOPMENT_MODE) {
-        console.log('Using sample data');
+    if (window.DB === undefined || DEVELOPMENT_MODE) {
+      console.log('Using sample data');
 
-        sampleCourses.map((course) => {
-            let courseID = course.courseID;
-            let program = course.program;
-            let capacity = course.capacity;
-            let number = course.number;
-            let name = course.name;
-            let credits = course.credits;
-            let id = Math.floor(Math.random() * 10000) + 1;
+      sampleCourses.map((course) => {
+        let courseID = course.courseID;
+        let program = course.program;
+        let capacity = course.capacity;
+        let number = course.number;
+        let name = course.name;
+        let credits = course.credits;
+        let id = Math.floor(Math.random() * 10000) + 1;
 
-            var newCourse = {id, program, number, name, courseID, credits, capacity};
-            stateCourses.push(newCourse);
+        var newCourse = { id, program, number, name, courseID, credits, capacity };
+        stateCourses.push(newCourse);
+      });
+      setCourses(stateCourses);
+    }
+    else {
+      //console.log(FETCH_ALL_COURSE_DATA);
+      // Send a query to main
+      window.DB.send(CHANNEL_COURSE_TO_MAIN, FETCH_ALL_COURSE_DATA); // Add constants
+
+      // Recieve the results
+      window.DB.receive(CHANNEL_COURSE_FROM_MAIN, (dataRows) => {
+        //console.log(dataRows);
+        //console.log(typeof dataRows);
+
+        dataRows.map((data) => {
+          let courseID = data.ClassID;
+          let program = data.dept_id;
+          let capacity = data.Capacity;
+          let number = data.class_num;
+          let credits = data.credits;
+          let name = data.class_name;
+          const id = Math.floor(Math.random() * 10000) + 1
+
+
+          let newCourse = { program, number, name, courseID, credits, capacity }; //This needs to be the same as onAddCourse() in CourseAddPage.js
+
+          stateCourses.push(newCourse);
         });
         setCourses(stateCourses);
-      }
-      else {
-        //console.log(FETCH_ALL_COURSE_DATA);
-        // Send a query to main
-        window.DB.send(CHANNEL_COURSE_TO_MAIN, FETCH_ALL_COURSE_DATA); // Add constants
-
-        // Recieve the results
-        window.DB.receive(CHANNEL_COURSE_FROM_MAIN, (dataRows) => {
-          //console.log(dataRows);
-          //console.log(typeof dataRows);
-
-          dataRows.map( (data) => {
-            let courseID = data.ClassID;
-            let program = data.dept_id;
-            let capacity = data.Capacity;
-            let number = data.class_num;
-            let credits = data.credits;
-            let name = data.class_name;
-            const id = Math.floor(Math.random() * 10000) + 1
-
-
-            let newCourse = {program, number, name, courseID, credits, capacity}; //This needs to be the same as onAddCourse() in CourseAddPage.js
-
-            stateCourses.push(newCourse);
-          }); 
-          setCourses(stateCourses);
-        });
-      }
+      });
+    }
   }
-  
+
   /**
    * Gets the latest data for professors.
    */
-   function getLatestProfessors() {
+  function getLatestProfessors() {
 
     let stateProfessors = [];
 
@@ -236,17 +236,17 @@ function App() {
       console.log('Using sample data');
 
       sampleProfessors.map((prof) => {
-          let name = prof.name;
-          let department = prof.department;
-          const id = Math.floor(Math.random() * 10000) + 1;
+        let name = prof.name;
+        let department = prof.department;
+        const id = Math.floor(Math.random() * 10000) + 1;
 
-          let newProfessor = {id, name, department};
-          stateProfessors.push(newProfessor);
+        let newProfessor = { id, name, department };
+        stateProfessors.push(newProfessor);
       });
       setProfessors(stateProfessors);
     }
     else {
-      
+
       // Send a query to main
       window.DB.send("toMain", FETCH_ALL_PROFESSOR_DATA);
 
@@ -254,102 +254,102 @@ function App() {
       window.DB.receive("fromMain", (dataRows) => {
         console.log(dataRows);
 
-        dataRows.map( (data) => {
+        dataRows.map((data) => {
           let name = data.ProfessorName;
           let department = '';
           const id = Math.floor(Math.random() * 10000) + 1;
 
-          let newProf = {id, name, department};
+          let newProf = { id, name, department };
           stateProfessors.push(newProf);
-          
-          
+
+
         });
         setProfessors(stateProfessors);
       });
     }
-}
+  }
 
-/**
- * Get the lates room data
- */
-function getLatestRooms() {
+  /**
+   * Get the lates room data
+   */
+  function getLatestRooms() {
 
-  let stateRooms = [];
+    let stateRooms = [];
 
-  // Update when DB team has implemented tables
-  if (window.DB === undefined || DEVELOPMENT_MODE || true) {
-    console.log('Using sample data');
+    // Update when DB team has implemented tables
+    if (window.DB === undefined || DEVELOPMENT_MODE || true) {
+      console.log('Using sample data');
 
-    sampleRooms.map((room) => {
-      let rbuilding = room.rbuilding;
-      let rnumber = room.rnumber;
-      let rcapacity = room.rcapacity;
-      let rtech = room.rtech;
+      sampleRooms.map((room) => {
+        let rbuilding = room.rbuilding;
+        let rnumber = room.rnumber;
+        let rcapacity = room.rcapacity;
+        let rtech = room.rtech;
         let id = Math.floor(Math.random() * 10000) + 1;
 
-        var newRoom = {id, rbuilding, rnumber, rcapacity, rtech,};
+        var newRoom = { id, rbuilding, rnumber, rcapacity, rtech, };
         stateRooms.push(newRoom);
-    })
-    setRooms(stateRooms);
-  }
-  else {
-    console.log(FETCH_ALL_ROOM_DATA);
+      })
+      setRooms(stateRooms);
+    }
+    else {
+      console.log(FETCH_ALL_ROOM_DATA);
 
-    window.DB.send("toMain", FETCH_ALL_ROOM_DATA);
+      window.DB.send("toMain", FETCH_ALL_ROOM_DATA);
 
-    window.DB.receive("fromMain", (dataRows) => {
-      console.log(dataRows);
-      console.log(typeof dataRows);
+      window.DB.receive("fromMain", (dataRows) => {
+        console.log(dataRows);
+        console.log(typeof dataRows);
 
-      dataRows.map( (data) => {
-        let rbuilding = data.rbuilding;
-        let rnumber = data.rnumber;
-        let rcapacity = data.rcapacity;
-        let rtech = data.rtech;
-        const id = Math.floor(Math.random() * 10000) + 1
+        dataRows.map((data) => {
+          let rbuilding = data.rbuilding;
+          let rnumber = data.rnumber;
+          let rcapacity = data.rcapacity;
+          let rtech = data.rtech;
+          const id = Math.floor(Math.random() * 10000) + 1
 
-        let newRoom = {rnumber}; 
+          let newRoom = { rnumber };
 
-        stateRooms.push(newRoom);
+          stateRooms.push(newRoom);
+        });
+        setCourses(stateRooms);
       });
-      setCourses(stateRooms);
+    }
+  }
+
+  function decribeDatabaseTable() {
+    window.DB.send(CHANNEL_COURSE_TO_MAIN, "Desc class");
+
+    window.DB.receive(CHANNEL_COURSE_FROM_MAIN, (data) => {
+      console.log(data);
     });
   }
-}
-
-function decribeDatabaseTable() {
-  window.DB.send(CHANNEL_COURSE_TO_MAIN, "Desc class");
-
-  window.DB.receive(CHANNEL_COURSE_FROM_MAIN, (data) => {
-    console.log(data);
-  });
-}
 
 
   //global styling
   const theme = createTheme({
-    palette: {primary: {main: "#90a4ae", dark:'#546e7a'}, secondary: {main: "#ffffff", dark:'#cfd8dc'}}
+    palette: { primary: { main: "#90a4ae", dark: '#546e7a' }, secondary: { main: "#ffffff", dark: '#cfd8dc' } }
   });
 
 
 
   return (
     <ThemeProvider theme={theme}>
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/course' element={<CoursePage onDelete={deleteCourse} onAddCourse={addCourse} courses={courses} programs={programs}/>} />
-          <Route path='/professor' element={<ProfessorPage onDelete={deleteProfessor} onAddProfessor={addProfessor} professors={professors} />} />
-          <Route path='/room' element={<RoomPage onDelete={deleteRoom} onAddRoom={addRoom} rooms={rooms} />} />
-          <Route path='/schedule' element={<Solution professors={professors} courses={courses} rooms={rooms}/>} />
-        </Routes>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/course' element={<CoursePage onDelete={deleteCourse} onAddCourse={addCourse} courses={courses} programs={programs} />} />
+            <Route path='/professor' element={<ProfessorPage onDelete={deleteProfessor} onAddProfessor={addProfessor} professors={professors} />} />
+            <Route path='/room' element={<RoomPage onDelete={deleteRoom} onAddRoom={addRoom} rooms={rooms} />} />
+            <Route path='/schedule' element={<Solution professors={professors} courses={courses} rooms={rooms} />} />
+          </Routes>
 
-        <button onClick={decribeDatabaseTable}>Get Table Details</button>
-      </div>
+          <button onClick={decribeDatabaseTable}>Get Table Details</button>
+        </div>
 
-      <div className='menu-container'><MenuBar/></div>
-    </Router>
+        <div className='menu-container'><MenuBar /></div>
+      </Router>
     </ThemeProvider>
   );
 }
