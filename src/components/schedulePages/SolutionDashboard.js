9ad224b2d26a5b234ac6f94eeaf6d822370c9cfb
Anshul Bharath <anshul.bharath@gmail.com>
2022-04-21 19:50:22 -0500

    Updating schedule dashboard with DB values.

--- src/components/schedulePages/SolutionDashboard.js
+++ src/components/schedulePages/SolutionDashboard.js
@@ -10,14 +10,15 @@ import TopBar from './../TopBar.js';
 import AddIcon from '@mui/icons-material/Add';
     /**
  * This component is a view that lists out individual LabListItems.
- * @param solutions - The state of solutions that are passed down from App.js
+ * @param plans - The state of plans that are passed down from App.js
  */
-     const SolutionList = ({solutions}) => {
+     const SolutionList = ({plans}) => {
+        console.log('SolutionList', plans);
         return(
         <div className='container'>
-            <h1>Solutions</h1>
-            {solutions.map((currentSolution, index) => (
-                <SolutionItem key={index} solutions={currentSolution}/>
+            <h1>plans</h1>
+            {plans.map((currentSolution, index) => (
+                <SolutionItem key={index} plan={currentSolution}/>
             ))}
         </div>
         );
@@ -25,14 +26,14 @@ import AddIcon from '@mui/icons-material/Add';
     
     /**
      * The component that will display an individual lab. These components will populate the LabList component.
-     * @param solutions - an individual solution
+     * @param plans - an individual solution
      */
-    const SolutionItem = ({solutions}) => {
-        {/*console.log(solutions);*/}
+    const SolutionItem = ({plan}) => {
+        {/*console.log(plans);*/}
         return(
         <div className='item'>
-            <h3>{solutions.name}</h3>
-            {solutions.data.map(solution => <div>{" professor: "}{solution.professor}{" course: "}{solution.course}{" time: "}{solution.time}{" room: "}{solution.room}</div>)}
+            <h3>{plan.planName}</h3>
+            <p>{plan.planDescription}</p>
             {/*What do we want this course part to show
             Maybe the course object that we can pop out?
             */}
@@ -43,18 +44,18 @@ import AddIcon from '@mui/icons-material/Add';
     /**
      * This page will have a list of Labs that have been added and
      * the labs that are in the database.
-     * @param solutions - the state of solutions passed from App.js
+     * @param plans - the state of plans passed from App.js
      */
-     const SolutionContent = ({ solutions }) => {
+     const SolutionContent = ({ plans }) => {
         return (
             <div className="home">
                 <div className='solution-page'>
-                    <SolutionList solutions={solutions}/>
+                    <SolutionList plans={plans}/>
                 </div>
             </div>
         );
     }
-const SolutionDashboard = ({solutions, setCurrentPage}) => {
+const SolutionDashboard = ({plans, setCurrentPage}) => {
     return (
         <div>
             <SideNavigation></SideNavigation>
@@ -64,7 +65,7 @@ const SolutionDashboard = ({solutions, setCurrentPage}) => {
                     <TopBar></TopBar>
 
                     <div className="container-home">
-                        <SolutionContent solutions={solutions}></SolutionContent>
+                        <SolutionContent plans={plans}></SolutionContent>
                     </div>
                     
                     {/* generate schedule button */}
